d1b5cc618783bcfafc4068a95c790b1cdde795b9
LizAraLeonGarcia <lizbeth.lgarcia@alumnos.udg.mx>
2024-09-25 15:57:44 -0600

    Primer commit

---
+++ database/migrations/2024_09_25_214722_create_productos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->decimal('precio', 8, 2);
+            $table->integer('cantidad');
+            $table->string('imagen')->nullable();
+            $table->unsignedBigInteger('vendedor_id'); // Llave foránea al usuario vendedor
+            $table->foreign('vendedor_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
