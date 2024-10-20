4b6a68db52a646989dd6aff5e7b468c29285fcd7
LizAraLeonGarcia <lizbeth.lgarcia@alumnos.udg.mx>
2024-10-19 21:33:15 -0600

    adicion de la fuente times new roman

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -3,9 +3,9 @@
 @section('title', 'Detalles del Producto')
 
 @section('content')
-    <div class="container">
-        <h2 class="text-center mb-4">Detalles del Producto</h2>
-        <div class="card shadow-lg border-light">
+    <div class="container-fluid" style="background-image: url('{{ asset('img/fondoShow.jpg') }}'); background-size: cover; background-position: center; background-repeat: no-repeat; height: 100vh;">
+        <h2 class="text-center mb-4" style="font-family: 'Times New Roman', serif;">Detalles del Producto</h2>
+        <div class="card shadow-lg border-light" style="font-family: 'Times New Roman', serif; background-color: rgba(255, 255, 255, 0.9);">
             <div class="card-header text-center bg-primary text-white">
                 <h3>{{ $producto->nombre }}</h3>
             </div>
@@ -14,9 +14,8 @@
                 <p><strong>Descripción:</strong> {{ $producto->descripcion }}</p>
                 <p><strong>Precio:</strong> ${{ number_format($producto->precio, 2) }}</p>
                 <p><strong>Cantidad:</strong> {{ $producto->stock }}</p>
-                <p><strong>Categoría:</strong> {{ $producto->categoria->nombre }}</p> <!-- Mostrar la categoría -->
+                <p><strong>Categoría:</strong> {{ $producto->categoria->nombre }}</p>
 
-                <!-- Muestra la imagen del producto -->
                 <div class="text-center mb-4">
                     @if($producto->imagen)
                         <img src="{{ asset('storage/' . $producto->imagen) }}" alt="{{ $producto->nombre }}" class="img-fluid rounded" style="max-width: 300px; height: auto;">
@@ -29,7 +28,6 @@
                     <a href="{{ route('productos.index') }}" class="btn btn-secondary">Volver a la lista</a>
                     <a href="{{ route('productos.edit', $producto->id) }}" class="btn btn-primary">Editar Producto</a>
 
-                    <!-- Botón para eliminar (si es apropiado) -->
                     <form action="{{ route('productos.destroy', $producto->id) }}" method="POST" class="d-inline-block">
                         @csrf
                         @method('DELETE')
